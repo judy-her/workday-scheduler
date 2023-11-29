c60a4efdacab98262a2deb4d0a6789418b3cfd4e
judy_her <judith85dm@ucla.edu>
2023-11-29 14:39:13 -0800

    event listener for save button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  $('.saveBtn').on('click', function () {
+    alert("Handler for the 'click' called.");
+  });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -33,6 +36,7 @@ $(function () {
 
     // Use a regular expression to match and extract numbers
     var numbers = idAttribute.match(/\d+/);
+    console.log(numbers); //this is an array
 
     // Check if numbers were found
     if (numbers) {
