462c2dc8b6a3fd9f4d32698a4b2fb9a43a938773
judy_her <judith85dm@ucla.edu>
2023-12-04 16:54:17 -0800

    deleted unnecessary comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,15 @@
 console.log('I am connected :)');
 var headerEl = $('#currentDay');
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
   getLocalStorage();
-  // TODO: 1- Add a listener for click events on the save button.
-  //   2-Local Storage- This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
+
   $('.saveBtn').on('click', setLocalStorage);
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   function displayTime() {
     var currentDay = dayjs().format('dddd, MMMM DD, YYYY hh:mm:ss a');
     headerEl.text(currentDay);
   }
-
-  // TODO: Add code to display the current date in the header of the page.
-  //   var currentDay = dayjs();
-  //   $('#currentDay').text(currentDay.format('dddd, MMMM DD, YYYY hh:mm:ss a'));
 
   var hour = dayjs();
   $('#currentHour').text(hour.hour()); //hour method gets current hour
@@ -71,45 +59,7 @@ $(function () {
     localStorage.setItem(timeBlockId, description);
   }
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   displayTime();
   //setinterval keeps time changing every second for seconds
   setInterval(displayTime, 1000);
 });
-
-//NOTE Local Storge ----------------------------------------
-// 2-Local Storage- This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-
-//NOTE original timeblocks ----------------------------------------
-// Get all elements with class "time-block"
-// var timeBlocks = document.querySelectorAll('.time-block');
-// // var timeBlocks = $('.time-block');
-
-// // Iterate over each element
-// timeBlocks.forEach(function (element) {
-//   // Get the id attribute of the current element
-//   var idAttribute = element.id;
-
-//   // Use a regular expression to match and extract numbers
-//   var numbers = idAttribute.match(/\d+/);
-
-//   // Check if numbers were found
-//   if (numbers) {
-//     // Convert the matched string to a number
-//     var extractedNumber = parseInt(numbers[0]);
-
-//     // Use the extracted number as needed
-//     console.log(extractedNumber);
-//     console.log('this is the hour now' + hourNow);
-//     // Example: Add a class based on the condition
-//     if (extractedNumber > hourNow) {
-//       element.classList.add('future');
-//     } else if (extractedNumber < hourNow) {
-//       element.classList.add('past');
-//     } else if (extractedNumber === hourNow) element.classList.add('present');
-//   }
-// });
